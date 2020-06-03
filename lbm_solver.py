c422d38793b1112607497ee3d0d626ceb977d4ba
etwll <hietwll@gmail.com>
2020-06-03 23:32:17 +0800

    encapsulate the solver by taichi data_oriented

--- lbm_solver.py
+++ lbm_solver.py
@@ -5,173 +5,177 @@ import matplotlib.pyplot as plt
 
 ti.init(arch=ti.gpu)
 
-nx = 256
-ny = 256
-dx = 1.0
-dy = 1.0
-rho0 = 1.0
-u_c = 0.1
-re = 1000.0
-lx = dx*float(nx-1)
-ly = dy*float(ny-1)
-niu = u_c*lx/re
-tau = 3.0*niu+0.5
+@ti.data_oriented
+class lbm_solver:
+    def __init__(self,nx,ny,niu,bc_type,bc_value,cy=0,cy_para=[0.0,0.0,0.0],steps=70000):
+        self.nx = nx
+        self.ny = ny
+        self.niu = niu
+        self.tau = 3.0*niu+0.5
+        self.inv_tau = 1.0/self.tau
+        self.rho = ti.var(dt=ti.f32, shape=(nx, ny))
+        self.vel = ti.Vector(2, dt=ti.f32, shape=(nx, ny))
+        self.display_var = ti.var(dt=ti.f32, shape=(nx, ny))        
+        self.f_old = ti.Vector(9, dt=ti.f32, shape=(nx, ny))
+        self.f_new = ti.Vector(9, dt=ti.f32, shape=(nx, ny))
+        self.w = ti.var(dt=ti.f32, shape=9)
+        self.e = ti.var(dt=ti.i32, shape=(9,2))
+        self.bc_type = ti.var(dt=ti.i32, shape=4)
+        self.bc_value = ti.var(dt=ti.f32, shape=(4,2))
+        self.cy = cy
+        self.cy_para = ti.var(dt=ti.f32, shape=3)
+        self.bc_type.from_numpy(np.array(bc_type,dtype=np.int32))
+        self.bc_value.from_numpy(np.array(bc_value,dtype=np.float32))
+        self.cy_para.from_numpy(np.array(cy_para,dtype=np.float32))
+        self.steps = steps
+        arr = np.array([4.0/9.0,1.0/9.0,1.0/9.0,1.0/9.0,1.0/9.0,
+                    1.0/36.0,1.0/36.0,1.0/36.0,1.0/36.0],dtype=np.float32)
+        self.w.from_numpy(arr)
+        arr = np.array([[0,0], [1,0], [0,1], [-1,0], [0,-1],
+                        [1,1], [-1,1], [-1,-1], [1,-1]],dtype=np.int32)
+        self.e.from_numpy(arr)
+
+    @ti.func
+    def f_eq(self,i,j,k):
+        eu = ti.cast(self.e[k,0],ti.f32)*self.vel[i,j][0] + ti.cast(self.e[k,1],ti.f32)*self.vel[i,j][1]
+        uv = self.vel[i,j][0]**2.0 + self.vel[i,j][1]**2.0
+        return self.w[k]*self.rho[i,j]*(1.0+3.0*eu+4.5*eu**2-1.5*uv)
+
+    @ti.kernel
+    def init(self):
+        for i,j in self.rho:
+            self.vel[i,j][0] = 0.0
+            self.vel[i,j][1] = 0.0
+            self.rho[i,j] = 1.0
+            for k in ti.static(range(9)):
+                 self.f_new[i,j][k] = self.f_eq(i,j,k)
+                 self.f_old[i,j][k] = self.f_new[i,j][k]
+
+    @ti.kernel             
+    def collide_and_stream(self):
+        for i,j in ti.ndrange((1,self.nx-1),(1,self.ny-1)):
+            for k in ti.static(range(9)):
+                ip = i - self.e[k,0]
+                jp = j - self.e[k,1]
+                self.f_new[i,j][k] = (1.0-self.inv_tau)*self.f_old[ip,jp][k] + \
+                                        self.f_eq(ip,jp,k)*self.inv_tau
+    @ti.kernel
+    def update_macro_var(self):
+        for i,j in ti.ndrange((1,self.nx-1),(1,self.ny-1)):
+            self.rho[i,j]= 0.0
+            self.vel[i,j][0] = 0.0
+            self.vel[i,j][1] = 0.0
+            for k in ti.static(range(9)):
+                self.f_old[i,j][k] = self.f_new[i,j][k]
+                self.rho[i,j] += self.f_new[i,j][k]
+                self.vel[i,j][0] += (ti.cast(self.e[k,0],ti.f32)*self.f_new[i,j][k])
+                self.vel[i,j][1] += (ti.cast(self.e[k,1],ti.f32)*self.f_new[i,j][k])
+
+            self.vel[i,j][0] /= self.rho[i,j]
+            self.vel[i,j][1] /= self.rho[i,j]
+
+    @ti.kernel
+    def apply_bc(self):
+        # left and right
+        for j in ti.ndrange(1,self.ny-1):
+            # left: dr = 0; ibc = 0; jbc = j; inb = 1; jnb = j
+            self.apply_bc_core(1,0,0,j,1,j)
+
+            # right: dr = 2; ibc = nx-1; jbc = j; inb = nx-2; jnb = j
+            self.apply_bc_core(1,2,self.nx-1,j,self.nx-2,j)
+
+        # top and bottom
+        for i in ti.ndrange(self.nx):
+            # top: dr = 1; ibc = i; jbc = ny-1; inb = i; jnb = ny-2
+            self.apply_bc_core(1,1,i,self.ny-1,i,self.ny-2)
+
+            # bottom: dr = 3; ibc = i; jbc = 0; inb = i; jnb = 1
+            self.apply_bc_core(1,3,i,0,i,1)
+
+        # cylindrical obstacle
+        if(self.cy==1):
+            for i,j in ti.ndrange(self.nx,self.ny):
+                i_=ti.cast(i,ti.f32)
+                j_=ti.cast(j,ti.f32)
+                if((i_- self.cy_para[0])**2.0+(j_- self.cy_para[1])**2.0<=self.cy_para[2]**2.0):
+                    self.vel[i,j][0] = 0.0 # velocity is zero at solid boundary
+                    self.vel[i,j][1] = 0.0
+                    inb = 0
+                    jnb = 0
+                    if(i_>=self.cy_para[0]):
+                        inb = i+1
+                    else:
+                        inb = i-1
+                    if(j_>=self.cy_para[1]):
+                        jnb = j+1
+                    else:
+                        jnb = j-1
+                    self.apply_bc_core(0,0,i,j,inb,jnb) 
 
 
-f_old = ti.Vector(9, dt=ti.f32, shape=(nx, ny))
-f_new = ti.Vector(9, dt=ti.f32, shape=(nx, ny))
-
-rho = ti.var(dt=ti.f32, shape=(nx, ny))
-vel = ti.Vector(2, dt=ti.f32, shape=(nx, ny))
-
-show_var = ti.var(dt=ti.f32, shape=(nx, ny))
-
-w = ti.Vector([4.0/9.0,1.0/9.0,1.0/9.0,1.0/9.0,1.0/9.0,
-               1.0/36.0,1.0/36.0,1.0/36.0,1.0/36.0])
-
-e = ti.Vector([[0,0], [1,0], [0,1], 
-    [-1,0], [0, -1], [1,1], [-1,1], [-1,-1], [1,-1]])
-
-
-# compute the distribution function at equilibrium state
-@ti.func
-def f_eq(ek0, ek1, wk, rho, u, v):
-    eu = ti.cast(ek0,ti.f32)*u + ti.cast(ek1,ti.f32)*v
-    uv = u*u + v*v
-    return wk*rho*(1.0+3.0*eu+4.5*eu**2-1.5*uv)
-
-
-@ti.kernel
-def init():
-    for i,j in rho:
-        vel[i,j][0] = 0.0
-        vel[i,j][1] = 0.0
-        rho[i,j] = rho0
+    @ti.func
+    def apply_bc_core(self,outer,dr,ibc,jbc,inb,jnb):
+        if(outer==1): # handle outer boundary
+            if(self.bc_type[dr]==0):
+                self.vel[ibc,jbc][0] = self.bc_value[dr,0]
+                self.vel[ibc,jbc][1] = self.bc_value[dr,1]
+            elif(self.bc_type[dr]==1):
+                self.vel[ibc,jbc][0] = self.vel[inb,jnb][0]
+                self.vel[ibc,jbc][1] = self.vel[inb,jnb][1]
+        self.rho[ibc,jbc] = self.rho[inb,jnb]
         for k in ti.static(range(9)):
-             f_new[i,j][k] = f_eq(e[k,0],e[k,1], w[k], rho[i,j], vel[i,j][0], vel[i,j][1])
-             f_old[i,j][k] = f_new[i,j][k]
+            self.f_old[ibc,jbc][k] = self.f_eq(ibc,jbc,k) - self.f_eq(inb,jnb,k) + \
+                                        self.f_old[inb,jnb][k]
 
-@ti.kernel             
-def collide_and_stream():
-    for i,j in ti.ndrange((1,nx-1),(1,ny-1)):
-        for k in ti.static(range(9)):
-            ip = i - e[k,0]
-            jp = j - e[k,1]
-            f_new[i,j][k] = (1.0-1.0/tau)*f_old[ip,jp][k] + \
-            f_eq(e[k,0],e[k,1], w[k], rho[ip,jp], vel[ip,jp][0], vel[ip,jp][1])/tau
+    @ti.kernel
+    def get_display_var(self,flg: ti.i32):
+        if(flg==0):
+            for i,j in ti.ndrange(self.nx,self.ny):
+                self.display_var[i,j] = ti.sqrt(self.vel[i,j][0]**2.0 + self.vel[i,j][1]**2.0)
+        elif(flg==1):
+            for i,j in ti.ndrange(self.nx,self.ny):
+                self.display_var[i,j] = self.vel[i,j][0]
 
-@ti.kernel
-def update_macro_var():
-    for i,j in ti.ndrange((1,nx-1),(1,ny-1)):
-        rho[i,j]= 0.0
-        vel[i,j][0] = 0.0
-        vel[i,j][1] = 0.0
-        for k in ti.static(range(9)):
-            f_old[i,j][k] = f_new[i,j][k]
-            rho[i,j] += f_new[i,j][k]
-            vel[i,j][0] += (ti.cast(e[k,0],ti.f32)*f_new[i,j][k])
-            vel[i,j][1] += (ti.cast(e[k,1],ti.f32)*f_new[i,j][k])
+    def solve(self):
+        gui = ti.GUI('lbm solver', (self.nx, self.ny))
+        self.init()
+        for i in range(self.steps):
+            self.collide_and_stream()
+            self.update_macro_var()
+            self.apply_bc()
+            if(i%5000==0):
+                print('Step: {:}'.format(i))
+                self.get_display_var(0)
+                gui.set_image(cm.plasma(self.display_var.to_numpy()/0.15))
+                gui.show()
 
-        vel[i,j][0] /= rho[i,j]
-        vel[i,j][1] /= rho[i,j]
-
-
-@ti.kernel
-def get_show_var():
-    for i,j in ti.ndrange(nx,ny):
-        show_var[i,j] = vel[i,j][0]/u_c
-
-@ti.kernel
-def apply_bc():
-    # left and right
-    for j in ti.ndrange(1,ny-1):
-        for k in ti.static(range(9)):
-
-            #left
-            ibc = 0
-            jbc = j
-            inb = 1
-            jnb = j
-
-            rho[ibc,jbc] = rho[inb,jnb]
-            f_old[ibc,jbc][k] = f_eq(e[k,0],e[k,1], w[k], rho[ibc,jbc], vel[ibc,jbc][0], vel[ibc,jbc][1]) - \
-                            f_eq(e[k,0],e[k,1], w[k], rho[inb,jnb], vel[inb,jnb][0], vel[inb,jnb][1]) + \
-                            f_old[inb,jnb][k]
-
-            #right
-            ibc = nx-1
-            jbc = j
-            inb = nx-2
-            jnb = j
-
-            rho[ibc,jbc] = rho[inb,jnb]
-            f_old[ibc,jbc][k] = f_eq(e[k,0],e[k,1], w[k], rho[ibc,jbc], vel[ibc,jbc][0], vel[ibc,jbc][1]) - \
-                            f_eq(e[k,0],e[k,1], w[k], rho[inb,jnb], vel[inb,jnb][0], vel[inb,jnb][1]) + \
-                            f_old[inb,jnb][k]
-
-    # top and bottom
-    for i in ti.ndrange(nx):
-        for k in ti.static(range(9)):
-
-            # bottom 
-            ibc = i
-            jbc = 0
-            inb = i
-            jnb = 1
-
-            rho[ibc,jbc] = rho[inb,jnb]
-            f_old[ibc,jbc][k] = f_eq(e[k,0],e[k,1], w[k], rho[ibc,jbc], vel[ibc,jbc][0], vel[ibc,jbc][1]) - \
-                            f_eq(e[k,0],e[k,1], w[k], rho[inb,jnb], vel[inb,jnb][0], vel[inb,jnb][1]) + \
-                            f_old[inb,jnb][k]
-
-            # top
-            ibc = i
-            jbc = ny-1
-            inb = i
-            jnb = ny-2
-
-            rho[ibc,jbc] = rho[inb,jnb]
-            vel[ibc,jbc][0] = u_c
-            f_old[ibc,jbc][k] = f_eq(e[k,0],e[k,1], w[k], rho[ibc,jbc], vel[ibc,jbc][0], vel[ibc,jbc][1]) - \
-                            f_eq(e[k,0],e[k,1], w[k], rho[inb,jnb], vel[inb,jnb][0], vel[inb,jnb][1]) + \
-                            f_old[inb,jnb][k]
-
-
-
-
+    def pass_to_py(self):
+        self.get_display_var(1)
+        return self.display_var.to_numpy()
 
 if __name__ == '__main__':
+    flow_case = 0
+    if(flow_case==0): # von Karman vortex street 
+        lbm = lbm_solver(401,101,0.0255,[0,0,1,0],
+                        [[0.1,0.0],[0.0,0.0],[0.0,0.0],[0.0,0.0]], # left, top, right, bottom
+                         0,[80.0,50.0,10.0])
+        lbm.solve()
+    elif(flow_case==1): # lid driven cavity flow
+        lbm = lbm_solver(256,256,0.0255,[0,0,0,0],[[0.0,0.0],[0.1,0.0],[0.0,0.0],[0.0,0.0]]) # left, top, right, bottom
+        lbm.solve()
 
-    gui = ti.GUI('LBM-Fluid', (nx, ny))
+        # compare with literature results
+        y_ref, u_ref = np.loadtxt('data/ghia1982.dat',unpack=True,skiprows=2,usecols=(0,2))
+        fig,axes = plt.subplots(nrows=1,ncols=1,figsize=(4,3),dpi=200)
+        axes.plot(np.linspace(0,1.0,256),lbm.pass_to_py()[256//2,:]/0.1,'b-',label='LBM')
+        axes.plot(y_ref,u_ref,'rs',label='Ghia et al. 1982')
+        axes.legend()
+        axes.set_xlabel(r'Y')
+        axes.set_ylabel(r'U')
+        plt.tight_layout()
+        plt.show()
 
-    init()
 
-    for i in range(20000):
-        collide_and_stream()
-        update_macro_var()
-        get_show_var()
-        apply_bc()
 
-        if(i%1000==0):
-            print('Step: {:}'.format(i))
-            img = show_var.to_numpy()
-            gui.set_image(cm.rainbow((img+0.5)/1.5))
-            gui.show()
-
-    img = show_var.to_numpy()
-
-    y_ref, u_ref = np.loadtxt('data/ghia1982.dat',unpack=True,skiprows=2,usecols=(0,2))
-
-    fig,axes = plt.subplots(nrows=1,ncols=1,figsize=(4,3),dpi=200)
-
-    axes.plot(np.linspace(0,1.0,ny),img[nx//2,:],'b-',label='LBM')
-    axes.plot(y_ref,u_ref,'rs',label='Ghia et al. 1982')
-    axes.legend()
-    axes.set_xlabel(r'Y')
-    axes.set_ylabel(r'U')
-
-    plt.tight_layout()
-
-    plt.show()
 
 
